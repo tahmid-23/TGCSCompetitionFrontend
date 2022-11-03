f70d30b895c7f577233d4b4a09479b9f874c876a
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-03 13:16:38 -0400

    remove create-react-app specific stuff, add linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'react-app'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+  env: {
+    es6: true
+  },
+  rules: {
+    'prettier/prettier': 2
+  }
+};
