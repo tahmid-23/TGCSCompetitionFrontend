37d59fa8e36c1100d8ff1038a1462d93f69f4c0a
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-09 18:11:08 -0500

    rename scripts, add sample css module

--- src/components/BulletedList/BulletedList.tsx
+++ src/components/BulletedList/BulletedList.tsx
@@ -1,8 +1,9 @@
 import { Children, PropsWithChildren } from 'react';
+import styles from './BulletedList.module.css';
 
 const BulletedList: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <ul>
+    <ul className={styles.smirkList}>
       {Children.map(children, (child) => {
         return <li>{child}</li>;
       })}
